c37a745531b0d31d9112fdb85b1989dfaf223a1d
s-saku <t.nishi7@gmail.com>
2020-12-25 18:25:43 +0900

    add test script and main process

--- lib/match_arrays.rb
+++ lib/match_arrays.rb
@@ -1,6 +1,59 @@
 require "match_arrays/version"
+require "active_record"
 
 module MatchArrays
   class Error < StandardError; end
-  # Your code goes here...
+
+  # A function that matches two arrays (masters, transactions) containing hash or ActiveRecord with the specified key.
+  # It judges "matching", "master only", or "transaction only", and then calls the callback Proc specified for each.
+  #
+  # * In this gem, the elements of the masters and transactions arrays are called "MA" and "TR" respectively.
+  # * Callbacks are accepted only for Proc, not for lambda, because lambda does not fit the strict error checking of block arguments.
+  # * Masters and transactions are replaced with an empty array only when nil is input.
+  #   This is because it is expected to work as intended in many cases.
+  def self.match(masters:, transactions:, p_m_key:, p_t_key:, p_match:, p_tr_only:, p_ma_only:, attr_obj: nil)
+    nil_to_array_masters = masters.nil? ? [] : masters
+    nil_to_array_transactions = transactions.nil? ? [] : transactions
+    raise ArgumentError, "Only an Array is accepted as the :masters argument." unless nil_to_array_masters.is_a?(Array) || nil_to_array_masters.is_a?(ActiveRecord::Associations::CollectionProxy)
+    raise ArgumentError, "Only an Array is accepted as the :transactions argument." unless nil_to_array_transactions.is_a?(Array) || nil_to_array_transactions.is_a?(ActiveRecord::Associations::CollectionProxy)
+    raise ArgumentError, "Only an Proc is accepted as the :p_m_key argument." unless p_m_key.is_a?(Proc)
+    raise ArgumentError, "Only an Proc is accepted as the :p_t_key argument." unless p_t_key.is_a?(Proc)
+    raise ArgumentError, "Only an Proc is accepted as the :p_match argument." unless p_match.is_a?(Proc)
+    raise ArgumentError, "Only an Proc is accepted as the :p_tr_only argument." unless p_tr_only.is_a?(Proc)
+    raise ArgumentError, "Only an Proc is accepted as the :p_ma_only argument." unless p_ma_only.is_a?(Proc)
+
+    # Generate a hash that can access MA with the key defined in p_m_key
+    ma_hash = nil_to_array_masters.each_with_object({}) do |ma, hash|
+      key = p_m_key.call(ma).to_s # Force to_s to avoid matching type errors
+      hash[key] = ma
+    end
+
+    # Create a hash with the same content as ma_hash.
+    # Deleting a MA that matches a TR from this hash, and determine that the remaining elements are "MA only"
+    ma_only_hash = nil_to_array_masters.each_with_object({}) do |ma, hash|
+      key = p_m_key.call(ma).to_s # Force to_s to avoid matching type errors
+      hash[key] = ma
+    end
+
+    # Create a hash that can access TR with the key defined in p_t_key
+    tr_hash = nil_to_array_transactions.each_with_object({}) do |tr, hash|
+      key = p_t_key.call(tr).to_s # Force to_s to avoid matching type errors
+      hash[key] = tr
+    end
+
+    # Read TRs one at a time and determine whether it is "matching" or "TR only"
+    tr_hash.each do |key, tr|
+      if ma_hash[key].present?
+        p_match.call(ma_hash[key], tr, attr_obj, nil_to_array_masters, nil_to_array_transactions)
+        ma_only_hash.delete(key)
+      else
+        p_tr_only.call(tr, attr_obj, nil_to_array_masters, nil_to_array_transactions)
+      end
+    end
+
+    # Call the "MA only" callback.
+    ma_only_hash.each_value do |ma|
+      p_ma_only.call(ma, attr_obj, nil_to_array_masters, nil_to_array_transactions)
+    end
+  end
 end
--- spec/match_arrays_spec.rb
+++ spec/match_arrays_spec.rb
@@ -1,9 +1,162 @@
 RSpec.describe MatchArrays do
-  it "has a version number" do
-    expect(MatchArrays::VERSION).not_to be nil
+  describe "Gem Basic Test" do
+    it "has a version number" do
+      expect(MatchArrays::VERSION).not_to be nil
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "Operation test" do
+    let!(:key_ma_only) { "key_ma_only" }
+    let!(:key_match) { "key_match" }
+    let!(:key_tr_only) { "key_tr_only" }
+  
+    let!(:value_ma1) { 1 }
+    let!(:value_ma2) { 2 }
+    let!(:value_tr1) { 3 }
+    let!(:value_tr2) { 4 }
+  
+    let(:masters) { [{ k: key_ma_only, v: value_ma1 }, { k: key_match, v: value_ma2 }] }
+    let(:transactions) { [{ k: key_match, v: value_tr1 }, { k: key_tr_only, v: value_tr2 }] }
+  
+    let(:p_m_key) { proc { |ma| ma[:k] } }
+    let(:p_t_key) { proc { |tr| tr[:k] } }
+  
+    let(:p_match) { proc {|m, t, obj| obj.push("#{output_str_match}#{m[:v] + t[:v]}") } }
+    let(:p_tr_only) { proc { |t, obj| obj.push("#{output_str_tr_only}#{t[:v]}") } }
+    let(:p_ma_only) { proc { |m, obj| obj.push("#{output_str_ma_only}#{m[:v]}") } }
+
+    let!(:attr_obj) { [] }
+
+    let!(:output_str_match) { "MATCH, Summarised value: " }
+    let!(:output_str_tr_only) { "TR only, TR value: " }
+    let!(:output_str_ma_only) { "MA only, MA value: " }
+
+    subject do
+      MatchArrays.match(
+        masters: masters,
+        transactions: transactions,
+        p_m_key: p_m_key,
+        p_t_key: p_t_key,
+        p_ma_only: p_ma_only,
+        p_tr_only: p_tr_only,
+        p_match: p_match,
+        attr_obj: attr_obj
+      )
+    end
+
+    describe "Argument Check" do
+      context "Nomal scenario" do
+        it "not raise error" do
+          expect{ subject }.not_to raise_error ArgumentError
+         end
+      end
+
+      context "Exception scenario" do
+        context "master is not an Array" do
+          let(:masters) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Array is accepted as the :masters argument."
+           end
+        end
+
+        context "transactions is not an Array" do
+          let(:transactions) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Array is accepted as the :transactions argument."
+           end
+        end
+
+        context "p_m_key is not an Proc" do
+          let(:p_m_key) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Proc is accepted as the :p_m_key argument."
+           end
+        end
+
+        context "p_t_key is not an Proc" do
+          let(:p_t_key) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Proc is accepted as the :p_t_key argument."
+           end
+        end
+
+        context "p_ma_only is not an Proc" do
+          let(:p_ma_only) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Proc is accepted as the :p_ma_only argument."
+           end
+        end
+
+        context "p_tr_only is not an Proc" do
+          let(:p_tr_only) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Proc is accepted as the :p_tr_only argument."
+           end
+        end
+
+        context "p_match is not an Proc" do
+          let(:p_match) { 1 }
+          it "raise error" do
+            expect{ subject }.to raise_error ArgumentError
+            expect{ subject }.to raise_error "Only an Proc is accepted as the :p_match argument."
+           end
+        end
+      end
+    end
+
+    describe "Matching process" do
+      context "maが0件" do
+        let(:masters) { [] }
+        it "'TR only' proc is only executed" do
+          expect{ subject }
+            .to change{ attr_obj[0] }.from(nil).to("#{output_str_tr_only}#{value_tr1}")
+            .and change{ attr_obj[1] }.from(nil).to("#{output_str_tr_only}#{value_tr2}")
+        end
+      end
+
+      context "transaction is empty" do
+        let(:transactions) { [] }
+        it "'MA only' proc is only executed" do
+          expect{ subject }
+            .to change{ attr_obj[0] }.from(nil).to("#{output_str_ma_only}#{value_ma1}")
+            .and change{ attr_obj[1] }.from(nil).to("#{output_str_ma_only}#{value_ma2}")
+        end
+      end
+
+      context "masters and transactions are both empty" do
+        let(:masters) { [] }
+        let(:transactions) { [] }
+        it "No proc is only executed" do
+          expect{ subject }.not_to change{ attr_obj }
+        end
+      end
+
+      context "masters and transactions are not both empty" do
+        context 'the first TR matches' do
+          it "The procs are executed in the order 'Match -> TR only -> MA only'" do
+            expect{ subject }
+              .to change{ attr_obj[0] }.from(nil).to("#{output_str_match}#{value_ma2 + value_tr1}")
+              .and change{ attr_obj[1] }.from(nil).to("#{output_str_tr_only}#{value_tr2}")
+              .and change{ attr_obj[2] }.from(nil).to("#{output_str_ma_only}#{value_ma1}")
+          end
+        end
+
+        context 'the second TR matches' do
+          let(:transactions) { [{ k: key_tr_only, v: value_tr1 }, { k: key_match, v: value_tr2 }] }
+          it "The procs are executed in the order 'TR only -> Match -> MA only'" do
+            expect{ subject }
+              .to change{ attr_obj[0] }.from(nil).to("#{output_str_tr_only}#{value_tr1}")
+              .and change{ attr_obj[1] }.from(nil).to("#{output_str_match}#{value_ma2 + value_tr2}")
+              .and change{ attr_obj[2] }.from(nil).to("#{output_str_ma_only}#{value_ma1}")
+          end
+        end
+      end
+    end
   end
 end
